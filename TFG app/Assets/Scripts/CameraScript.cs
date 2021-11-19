929169c280e1655d09de238f1b61b95e15fbe488
Alejandro Granell <alejandrogranell@outlook.es>
2021-11-19 17:51:08 +0100

    ui prueba camara

---
+++ TFG app/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitiEngine.UI;
+
+
+public class CameraScript : MonoBehaviour
+{
+
+    int currentCamIndex = 0;
+    WebCamTexture tex;
+
+    public RawImage display;
+
+    public void SwapCam_Clicked() {
+
+    }
+
+    public void StartStopCam_Clicked() {
+
+    }
+  
+}
