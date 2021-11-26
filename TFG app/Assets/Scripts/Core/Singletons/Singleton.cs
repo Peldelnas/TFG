05dc8a836da0c430326597c73666db26c9252629
Alejandro Granell <alejandrogranell@outlook.es>
2021-11-26 19:40:14 +0100

    Anim to do + CanvasController

---
+++ TFG app/Assets/Scripts/Core/Singletons/Singleton.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace RestClient.Core.Singletons
+{
+    public class Singleton<T> : MonoBehaviour
+        where T : Component
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    var objs = FindObjectsOfType(typeof(T)) as T[];
+                    if (objs.Length > 0)
+                        _instance = objs[0];
+                    if (objs.Length > 1)
+                    {
+                        Debug.LogError("There is more than one " + typeof(T).Name + " in the scene.");
+                    }
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.name = string.Format("_{0}", typeof(T).Name);
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+        
+        //protección
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (_instance != this as T)
+            {
+                Destroy(gameObject);
+            }
+            else { DontDestroyOnLoad(gameObject); }
+        }
+    }
+}
