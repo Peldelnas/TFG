3a3423e8631b480dab25cbd98039a942c34176aa
Alejandro Granell <alejandrogranell@outlook.es>
2021-12-02 02:12:32 +0100

    assets generated

--- TFG app/Assets/Scripts/SoundManager.cs
+++ TFG app/Assets/Scripts/SoundManager.cs
@@ -1,18 +1,14 @@
+using RestClient.Core.Singletons;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager : Singleton<SoundManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    AudioSource[] sounds;
+    protected override void Awake()
     {
-        
+        sounds = GetComponents<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
